394e64f6e01de40dd4f8ee53b83d287b7861e8c7
David Medina <david00medina@gmail.com>
2017-03-02 17:10:41 +0000

    SortedVector v1.0 -----------------
    We have implemented some tests to check the functionality of a Sorted 
    Vector data structure. Insertion and Extraction are implemented.
    
    TODO Searching tests

---
+++ src/practica2/ContenedorDeEnteros.java
@@ -0,0 +1,69 @@
+package practica2;
+
+
+public class ContenedorDeEnteros {
+    private int[] vector;
+    private int length;
+
+    public ContenedorDeEnteros(int length) {
+        vector = new int[length];
+        this.length = 0;
+    }
+    
+    public int cardinal() {
+        return length;
+    }
+    
+    public int[] elementos() {
+        int[] outVec = new int[length];
+        for (int i = 0; i < length; i++) {
+            outVec[i] = vector[i];
+        }
+        return outVec;
+    }
+    
+    public void vaciar() {
+        vector = null;
+    }
+    
+    public boolean extraer(int e) {
+        if(!buscar(e) || length == 0)
+            return false;
+        
+        int i = 0;
+        while((vector[i] != e) && (i < cardinal())) {
+            i++;
+            if(vector[i] == e) {
+                while(i < cardinal() - 1) {
+                    vector[i] = vector[i+1];
+                }
+                i++;
+            }
+        }
+        length--;
+        return true;
+    }
+    
+    public boolean insertar(int e) {
+        if(buscar(e) || length == vector.length)
+            return false;
+        
+        int i = 0;
+        while((vector[i] < e) && (i < vector.length)) {
+            
+            if(vector[i] > e) {
+                while(i < vector.length - 1) {
+                    int aux = vector[i];
+                    
+                }
+            }
+        }
+        length++;
+        return true;
+    }
+
+    private boolean buscar(int e) {
+        return false;
+    }
+    
+}
--- src/practica2/PruebaContenedor.java
+++ src/practica2/PruebaContenedor.java
@@ -1,27 +1,97 @@
 package practica2;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 
 public class PruebaContenedor {
     
-    public static void main(String[] args) {
+    private static final String[] CHARCOUNT = {"primeras","segundas","terceras",
+                                            "cuartas","quintas","sextas","séptimas",
+                                            "octavas","novenas","útimas"};
+            
+    public static void main(String[] args) throws IOException {
         Time time = new Time();
+        RandomAccessFile dataFile = new RandomAccessFile("datos.dat","r");
+        BufferedWriter outFile = new BufferedWriter(new FileWriter("salida2.txt"));
+        ContenedorDeEnteros sortedVec = new ContenedorDeEnteros(100000);
+        
+        outFile.write("[+] Iniciando prueba de inserción : ");
+        outFile.newLine();
+        outFile.newLine();        
+        
+        insertionTest(dataFile, outFile, sortedVec, time);
+        time = new Time();
+        
+        outFile.newLine();
+        outFile.newLine();
+        outFile.newLine();
+        
+        outFile.write("[+] Iniciando prueba de extracción : ");
+        outFile.newLine();
+        outFile.newLine();
+        
+        extractionTest(dataFile, outFile, sortedVec, time);
+        time = new Time();
         
         for (int i = 0; i < 10; i++) {
             time.startTime(i);
-            System.out.println(i + " tiempo inicial : " + (double)time.getTime(i));
+            System.out.println(i + " tiempo inicial : " + time.getTime(i));
             for (long j = 0; j < (long)100000000; j++) {
                 
             }
             time.stopTime(i);
-            System.out.println(i + " tiempo final : " + (double)time.getTime(i) + "\n");
+            System.out.println(i + " tiempo final : " + time.getTime(i) + "\n");
         }
         System.out.println(time.meanTime());
+        outFile.close();
+    }
+    
+    
+    private static void insertionTest(RandomAccessFile dataFile,
+                                    BufferedWriter outFile,
+                                    ContenedorDeEnteros sortedVec, 
+                                    Time time) throws IOException {
+        
+        for(int i = 0; i < 10; i++) {
+            outFile.write("Resultado de las " + CHARCOUNT[i] + " 10.000 inserciones : ");
+            outFile.write("-----------------------------------------------------------");
+            time.startTime(i);
+            
+            for(int j = 0; j < 10000; j++)
+                sortedVec.insertar(dataFile.readInt());
+            
+            time.stopTime(i);
+            outFile.write("   [+] Tiempo de inserción : " + time.getTime(i) + " ms");
+            outFile.write("   [+] Tiempo medio de inserción : " + time.meanTime() + " ms");            
+        }
+                
+    }
+
+    private static void extractionTest(RandomAccessFile dataFile, 
+                                    BufferedWriter outFile, 
+                                    ContenedorDeEnteros sortedVec, 
+                                    Time time) throws IOException {
+        
+        for(int i = 0; i < 10; i++) {
+            outFile.write("Resultado de las " + CHARCOUNT[i] + " 10.000 extracciones : ");
+            outFile.write("-----------------------------------------------------------");
+            time.startTime(i);
+            
+            for(int j = 0; j < 10000; j++)
+                sortedVec.insertar(dataFile.readInt());
+            
+            time.stopTime(i);
+            outFile.write("   [+] Tiempo de extracción : " + time.getTime(i) + " ms");
+            outFile.write("   [+] Tiempo medio de extracción : " + time.meanTime() + " ms");            
+        }
     }
     
     
     private static class Time{
-        private ArrayList<Long> time;
+        private final ArrayList<Long> time;
 
         private Time() {
             time = new ArrayList<>();
@@ -32,7 +102,7 @@ public class PruebaContenedor {
         }
         
         private void startTime(int i) {
-            time.add(i, Long.valueOf(System.currentTimeMillis()));
+            time.add(i, System.currentTimeMillis());
         }
         
         private void stopTime(int i) {
@@ -48,7 +118,8 @@ public class PruebaContenedor {
         }
         
         private double meanTime() {
-            return (double) totalTime()/(time.size()*1000);
+            return totalTime()/time.size();
         }
     }
+    
 }
